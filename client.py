b9d57e11ee6c7ab582170cc43e70a5b9664d9b84
Pere Piñol <ppp3@alumnes.udl.cat>
2018-06-14 13:53:09 +0200

    Implemented file sending feature.

--- client.py
+++ client.py
@@ -3,8 +3,26 @@ import sys
 from select import select
 import socket
 
-PORT = 2000
+PORT = 2001
 T = 5
+
+QUIT = 0
+CWD = 1
+SEND = 2
+RECV = 3
+
+class struct:
+    @staticmethod
+    def pack(command, data):
+        string = str(command) + data
+        while (len(string) < 512):
+            string += '\x00'
+        print string
+        return string
+
+    @staticmethod
+    def unpack(packet):
+        return (int(packet[0]), packet[1:].rstrip('\x00'))
 
 
 def print_help():
@@ -12,14 +30,78 @@ def print_help():
 
 
 def get_server_cwd(sock):
-    sock.send("cwd")
+    sock.send(struct.pack(CWD, ""))
     (r, _, _) = select([sock], [], [], T)
     if (sock in r):
-        path = sock.recv(512).strip()
+        _, path = struct.unpack(sock.recv(512))
         return path
     else:
         print("Lost connection to server")
         exit(1)
+
+
+def send_packet(sock, string):
+    packet = struct.pack(SEND, string)
+    sent = 0
+    while (sent < len(packet)):
+        sent += sock.send(packet[sent:])
+
+
+def send(sock, origin, dest):
+    try:
+        fh = open(origin, "r")
+    except:
+        print("File " + origin + " doesn't exist!")
+        return
+
+    print("Sending file: " + origin + " to " + dest)
+    send_packet(sock, dest)
+    content = None
+    while (content != ""):
+        content = fh.read(511)
+        send_packet(sock, content)
+
+    print("File sent correctly")
+
+
+def recv(origin, dest):
+    print("Receiving file: " + origin + " to " + dest)
+
+
+def run_command(sock, command, path):
+    if (command[0] == "exit"):
+        sock.send(struct.pack(QUIT, ""))
+        return (True, path)
+
+    elif (command[0] == "help"):
+        print_help()
+        return (False, path)
+
+    elif (command[0] == "send"):
+        if (len(command) >= 3):
+            send(sock, path + "/" + command[1], command[2])
+            return (False, path)
+
+    elif (command[0] == "recv"):
+        if (len(command) >= 3):
+            recv(sock, command[1], path + "/" + command[2])
+            return (False, path)
+
+    elif (command[0] == "cd"):
+        print("Not implemented yet")
+        return (False, path)
+
+    elif (command[0] == "pwd"):
+        if (len(command) >= 2):
+            if (command[1] == "client"):
+                print(path)
+                return (False, path)
+            elif (command[1] == "server"):
+                print(get_server_cwd(sock))
+                return (False, path)
+
+    print("Invalid command: " + " ".join(command))
+    return (False, path)
 
 
 if __name__ == "__main__":
@@ -29,22 +111,13 @@ if __name__ == "__main__":
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((sys.argv[1], PORT))
 
-    clientpath = os.getcwd()
-    serverpath = get_server_cwd(sock)
+    path = os.getcwd()
     quit = False
     while(not quit):
         (stdin, _, _) = select([sock, 1], [], [])
         if (1 in stdin):
             command = os.read(1, 512).strip().split()
-            if (command[0] == "exit"):
-                quit = True
-                sock.send("exit")
-            elif (command[0] == "help"):
-                print_help()
-            elif (command[0] == "send"):
-                print("Sending file: " + clientpath + "/" + command[1] + " to " + serverpath + "/" + command[2])
-            elif (command[0] == "recv"):
-                print("Receiving file: " + serverpath + "/" + command[1] + " to " + clientpath + "/" + command[2])
+            quit, path = run_command(sock, command, path)
 
     print("Everything done")
     exit(0)
--- server.py
+++ server.py
@@ -3,21 +3,64 @@ import os
 import signal
 from select import select
 
-PORT = 2000
+PORT = 2001
+T = 5
+
+QUIT = 0
+CWD = 1
+SEND = 2
+RECV = 3
+
+class struct:
+    @staticmethod
+    def pack(command, data):
+        string = str(command) + data
+        while (len(string) < 512):
+            string += '\x00'
+        return string
+
+    @staticmethod
+    def unpack(packet):
+        return (int(packet[0]), packet[1:].rstrip('\x00'))
 
 
 def manageConnection(socket):
     quit = False
+    path = os.getcwd()
     while (not quit):
         (r, _, _) = select([socket], [], [])
-        command = socket.recv(512).strip().split()
-        if (command[0] == "exit"):
+        command, data = struct.unpack(socket.recv(512))
+        print command, data
+        if (command == QUIT):
             quit = True
-        elif (command[0] == "cwd"):
-            socket.send(os.getcwd())
-        elif (command[0] == "send"):
-            pass  # Receive packet (fragmented if necessary)
-        elif (command[0] == "recv"):
+            print("Finishing connection: " + ip[0])
+
+        elif (command == CWD):
+            socket.send(struct.pack(CWD, path))
+
+        elif (command == SEND):
+            fh = open(path + "/" + data, "w")
+            print("Opened " + path + "/" + data)
+            r, _, _ = select([socket], [], [], T)
+            if (socket not in r):
+                print("Lost connection with client")
+                exit(1)
+            command, data = struct.unpack(socket.recv(512))
+
+            while (data != ""):
+                if (command != SEND):
+                    print("Error receiving data")
+                    exit(1)
+
+                fh.write(data)
+
+                r, _, _ = select([socket], [], [], T)
+                if (socket not in r):
+                    print("Lost connection with client")
+                    exit(1)
+                command, data = struct.unpack(socket.recv(512))
+
+        elif (command == RECV):
             pass  # Send packet (fragmented if necessary)
     exit(0)
 
@@ -32,7 +75,4 @@ if(__name__ == "__main__"):
             manageConnection(connection)
         else:
             print("Connection received! " + ip[0])
-            incoming.close()
-            connection.close()
-            exit(0)
     exit(1)
